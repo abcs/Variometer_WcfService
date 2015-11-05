0cf189f78fb7c3afb4b605c562a65f54be135f84
abcs <abcs2@freemail.hu>
2015-11-05 22:20:57 +0100

    First commit

---
+++ Altimeter_WcfService/IService1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfJsonRestService
+{
+    public class Service1 : IService1
+    {
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "data/{id}")]
+        public Person GetData(string id)
+        {
+            // lookup person with the requested id 
+            return new Person()
+            {
+                Id = Convert.ToInt32(id),
+                Name = "Leo Messi"
+            };
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Altimeter_WcfService/Service1.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfJsonRestService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        Person GetData(string id);
+    }
+}
